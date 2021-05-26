7586d36cc53c58c1f60c8c1dc48804b3f1a3fd12
feliciovcm <feliciovcm@gmail.com>
2021-05-25 21:17:20 -0300

    Add apex chart and dynamic redenring

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,34 +1,65 @@
 import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/layout";
-import { ResponsiveBump } from "@nivo/bump";
+import dynamic from "next/dynamic";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
-const dataFn = [
-  {
-    id: "Semana 24/05",
-    data: [
-      {
-        x: 'segunda',
-        y: 6,
-      },
-      {
-        x: 'terça',
-        y: 110,
-      },
-      {
-        x: 'quarta',
-        y: 151,
-      },
-      {
-        x: 'quinta',
-        y: 200,
-      },
-      {
-        x: 'sexta',
-        y: 71,
-      },
+import { theme } from "../styles/theme";
+// Como o html da página no next é formado na camada intermediária. E o grafico da apexcharts é
+// formado somente no browser, no frontend. Importar da maneira comum, não vai dar certo, logo
+// teremos que fazer da seguinte maneira:
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+// Esse dynamic é utilizado para fazer lazy load, carregar componente de forma dinamica.
+// Exemplo: Vou carregar um componente somente quando o usuario clicar em um botão.
+// Passando ssr: false, eu garato que o gráfico será renderizado somente pelo lado do browser,
+// e nunca pelo lado do servidor, garantindo seu funcionamento.
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: { show: false },
+  dataLabels: { enabled: false },
+  tooltip: { enabled: false },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2021-03-18T00:00:00.000Z",
+      "2021-03-19T00:00:00.000Z",
+      "2021-03-20T00:00:00.000Z",
+      "2021-03-21T00:00:00.000Z",
+      "2021-03-22T00:00:00.000Z",
+      "2021-03-23T00:00:00.000Z",
+      "2021-03-24T00:00:00.000Z",
     ],
   },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [
+  {
+    name: "series1",
+    data: [31, 78, 291, 57, 120, 763, 267],
+  },
 ];
+
 export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
@@ -38,49 +69,17 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} h="300px">
+          <Box p="8" bg="gray.800" borderRadius={8} h="300px" pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
-            <ResponsiveBump
-              data={dataFn}
-              margin={{ top: 10, right: 60, bottom: 40, left: 60 }}
-              colors={{ scheme: "spectral" }}
-              lineWidth={3}
-              activeLineWidth={6}
-              inactiveLineWidth={3}
-              inactiveOpacity={0.15}
-              pointSize={10}
-              activePointSize={16}
-              inactivePointSize={0}
-              pointColor={{ theme: "background" }}
-              pointBorderWidth={3}
-              activePointBorderWidth={3}
-              pointBorderColor={{ from: "serie.color" }}
-              axisTop={null}
-              axisRight={null}
-              axisBottom={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: "",
-                legendPosition: "middle",
-                legendOffset: 32,
-              }}
-              axisLeft={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: "Inscritos",
-                legendPosition: "middle",
-                legendOffset: -40,
-              }}
-            />
+            <Chart type="area" height={160} options={options} series={series} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de Abertura
             </Text>
+            <Chart type="area" height={160} options={options} series={series} />
           </Box>
         </SimpleGrid>
       </Flex>
